13a117ac3950f781808e20d6f4d216a23f20c2cb
Greg Pina <pinagm2825@gmail.com>
2020-07-13 12:57:54 -0400

    configuring google auth login

--- config/passport.js
+++ config/passport.js
@@ -11,11 +11,31 @@ module.exports = function (passport) {
         callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value
+        }
+
+        try {
+          let user = await User.findOne({ googleId: profile.id})
+          if (user) {
+            done(null, user)
+          } else {
+            user = await User.create(newUser)
+            done(null,user)
+          }
+        } catch(err) {
+          console.error(err)
+        }
       }
     )
   )
-  passport.serializeUser((user, done) => done(null, user.id));
+  passport.serializeUser((user, done) => 
+    done(null, user.id)
+  )
 
   passport.deserializeUser((id, done) =>
     User.findById(id, (err, user) => done(err, user))
--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,25 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 
-// @desc Auth with Google
-//@route GET /auth/google
-router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-// @desc Google auth callback
-//@route GET /auth/google/callback
-router.get('/google/callback',passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
     res.redirect('/dashboard')
-})
+  }
+)
 
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/auth/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
 module.exports = router
