7a24119fc201265853d1531ea88600b330e14080
Greg Pina <pinagm2825@gmail.com>
2020-07-13 12:05:43 -0400

    Google Authentication strategy and callback

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const connect = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
 
 // Load config 
 dotenv.config({path: './config/config.env'})
+
+//Pasport config
+require('./config/passport')(passport)
 
 //call the database
 connectDB()
@@ -20,7 +26,21 @@ if(process.env.NODE_ENV === 'development') {
 
 //Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
-app.set('view engine', '.hbs;')
+app.set('view engine', '.hbs')
+
+//Sessions middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const router = express.Router()
 // @desc Login/Landing page
 //@route GET /
 router.get('/', (req,res) => {
-    res.send('Login')
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
 // @desc Dashboard
 //@route GET /dashboard
 router.get('/dashboard', (req,res) => {
-    res.send('Dashboard')
+    res.render('dashboard')
 })
 
 
